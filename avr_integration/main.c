a47d9b5815d741d088a05f5b29cdd09d9f2886c8
Maxim Kulakovskiy <mkulakovskiy>
2017-02-25 16:18:25 +0100

    Initial commit

---
+++ avr_integration/main.c
@@ -0,0 +1,18 @@
+/*
+ * avr_integration.c
+ *
+ * Created: 2/25/2017 3:00:01 PM
+ * Author : admin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
